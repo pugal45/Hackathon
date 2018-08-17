7c69a0f18a6a6e1a1e0f7547ca0e473e02b62aea
pugal45 <42461374+pugal45@users.noreply.github.com>
2018-08-17 02:14:19 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+console.log("JavaScript is amazing!");
+function myFunction(){
+var name = document.getElementById("name").value;
+document.getElementById("demo").innerHTML = "hello" + " "  + name;
+}
